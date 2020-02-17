483e950f5c532c8d72880e778b3d0d06cab2004b
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-02-17 18:24:33 +0900

    httpURLRequest추가

--- Android_APP/app/src/main/java/com/smuemsw/capstone/RemoteLogoutActivity.java
+++ Android_APP/app/src/main/java/com/smuemsw/capstone/RemoteLogoutActivity.java
@@ -2,13 +2,63 @@ package com.smuemsw.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.URL;
 
 public class RemoteLogoutActivity extends AppCompatActivity {
+
+    TextView logoutTextView;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_logout);
+
+        logoutTextView = (TextView) findViewById(R.id.remoteLogoutTextView);
+        logoutButton = (Button) findViewById(R.id.remoteLogoutButton);
+
+        //URL 설정.
+        String url = "http://18.221.178.47:8080/";
+
+        // AsyncTask를 통해 HttpURLConnection 수행.
+        NetworkTask networkTask = new NetworkTask(url, null);
+        networkTask.execute();
+
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpConnection requestHttpURLConnection = new RequestHttpConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            logoutTextView.setText(s);
+        }
     }
 }
