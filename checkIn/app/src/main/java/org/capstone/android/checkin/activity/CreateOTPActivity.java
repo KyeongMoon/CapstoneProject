f9cac101c5438357a8c9e5462334cb3ab34200a0
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2020-02-28 22:14:40 +0900

    로컬에서 otp 테스트
    로컬에서 otp 테스트

--- checkIn/app/src/main/java/org/capstone/android/checkin/activity/CreateOTPActivity.java
+++ checkIn/app/src/main/java/org/capstone/android/checkin/activity/CreateOTPActivity.java
@@ -1,18 +1,31 @@
 package org.capstone.android.checkin.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaDrm;
+import android.media.UnsupportedSchemeException;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import org.capstone.android.checkin.R;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Permission;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.UUID;
 
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
@@ -27,18 +40,36 @@ public class CreateOTPActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_otp);
 
 
+
+
+
+        //키 생성인데 사실상 필요없고 기기값 가져와서 전송해주자.
         HashMap map = this.generate("user", "host");
 
         TextView textView = findViewById(R.id.createOTPTextView);
         textView.setText(map.get("encodedKey").toString());
 
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
-//        for (int i = 0; i < 100000; i++) {
-//            String a = Integer.toString(i);
-            if (checkCode("123456" , map.get("encodedKey").toString())) {
-                textView.setText("success");
-//            }
+        UUID WIDEVINE_UUID = new UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L);
+        MediaDrm wvDrm;
+        try {
+
+            wvDrm = new MediaDrm(WIDEVINE_UUID);
+
+            //이제부터 사용할 키값
+            byte[] widevineId = wvDrm.getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID);
+            String aa = widevineId.toString();
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                String encodedWidevineId = Base64.getEncoder().encodeToString(widevineId).trim();
+
+                textView.setText("start " +widevineId.length+"\n"+ widevineId +"\n" + aa +"\n");
+            }
+        } catch (UnsupportedSchemeException e) {
+            e.printStackTrace();
         }
+
     }
 
     public HashMap<String, String> generate(String userName, String hostName) {
@@ -61,20 +92,20 @@ public class CreateOTPActivity extends AppCompatActivity {
         return map;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public boolean checkCode(String userCode, String otpkey) {
         long otpnum = Integer.parseInt(userCode);
         long wave = new Date().getTime() / 30000;
         boolean result = false;
         try {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                Base64.Decoder decoder = Base64.getDecoder();
-                byte[] decodedKey = decoder.decode(otpkey);
-                int window = 3;
-                for (int i = -window; i <= window; i++) {
-                    long hash = verify_code(decodedKey, wave + i);
-                    if (hash == otpnum) result = true;
-                }
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] decodedKey = decoder.decode(otpkey);
+            int window = 3;
+            for (int i = -window; i <= window; i++) {
+                long hash = verify_code(decodedKey, wave + i);
+                if (hash == otpnum) result = true;
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
